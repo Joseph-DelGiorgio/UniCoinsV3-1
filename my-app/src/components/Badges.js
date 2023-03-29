98af47b9f2513c87be4a976850fd0d87cfc093e8
Joseph Del Giorgio <106563909+Joseph-DelGiorgio@users.noreply.github.com>
2023-03-28 21:38:48 -0700

    Update Badges.js

--- my-app/src/components/Badges.js
+++ my-app/src/components/Badges.js
@@ -6,26 +6,28 @@ import { CONTRACT_ABI, CONTRACT_ADDRESS } from './constants';
 
 const Badges = ({ provider, volunteerAddress }) => {
   const [badges, setBadges] = useState([]);
+  const [filter, setFilter] = useState("all");
+  const [searchTerm, setSearchTerm] = useState("");
 
   useEffect(() => {
     const fetchBadges = async () => {
       if (!provider || !volunteerAddress) return;
-  
+
       const contractAddress = CONTRACT_ADDRESS;
-        
+
       const contract = new ethers.Contract(contractAddress, UNCollaboration.abi, provider);
-  
+
       const badgeAddresses = await contract.getProjectManagerAddresses();
-  
+
       const fetchedBadges = [];
       for (const badgeAddress of badgeAddresses) {
         const [badgeDescription, hoursContributed] = await contract.getBadge(volunteerAddress, badgeAddress);
         fetchedBadges.push({ badgeDescription, hoursContributed });
       }
-  
+
       setBadges(fetchedBadges);
     };
-  
+
     fetchBadges();
   }, [provider, volunteerAddress]);
 
@@ -38,26 +40,73 @@ const Badges = ({ provider, volunteerAddress }) => {
       return 'gold';
     }
   };
-  
-  return (
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value);
+    };
+    
+    const handleSearchTermChange = (event) => {
+    setSearchTerm(event.target.value);
+    };
+    
+    const filteredBadges = badges.filter((badge) => {
+      if (filter === "all") return true;
+      if (filter === "bronze") return badge.hoursContributed < 50;
+      if (filter === "silver") return badge.hoursContributed >= 50 && badge.hoursContributed < 100;
+      if (filter === "gold") return badge.hoursContributed >= 100;
+      if (filter === "humanitarian") return badge.badgeDescription.toLowerCase().includes("humanitarian");
+      if (filter === "environmental") return badge.badgeDescription.toLowerCase().includes("environmental");
+      if (filter === "educational") return badge.badgeDescription.toLowerCase().includes("educational");
+      return false;
+    });
+    
+    const searchedBadges = filteredBadges.filter((badge) => {
+    return badge.badgeDescription.toLowerCase().includes(searchTerm.toLowerCase());
+    });
+    
+    return (
     <div className="container">
-      <h2>Badges</h2>
-      {badges.length === 0 ? (
-        <p>No badges found</p>
-      ) : (
-        <div className="badge-container">
-          {badges.map((badge, index) => (
-            <div key={index} className={`badge-card ${mapBadgeToColor(badge.hoursContributed)}`}>
-              <img src="https://via.placeholder.com/100" alt="Badge" />
-              <div className="badge-name">{badge.badgeDescription}</div>
-              <div className="badge-description">Description: {badge.badgeDescription}</div>
-              <div className="badge-hours">Hours Contributed: {badge.hoursContributed}</div>
-            </div>
-          ))}
+    <h2>Badges</h2>
+    <div className="filter-container">
+    <label htmlFor="filter">Filter by: </label>
+    <select id="filter" value={filter} onChange={handleFilterChange}>
+    <option value="all">All</option>
+    <option value="bronze">Bronze</option>
+    <option value="silver">Silver</option>
+    <option value="gold">Gold</option>
+    <option value="humanitarian">Humanitarian</option>
+    <option value="environmental">Environmental</option>
+    <option value="educational">Educational</option>
+    </select>
+  </div>
+  <div className="search-container">
+    <input
+      type="text"
+      placeholder="Search badges..."
+      value={searchTerm}
+      onChange={handleSearchTermChange}
+    />
+  </div>
+  {searchedBadges.length === 0 ? (
+    <p>No badges found</p>
+  ) : (
+    <div className="badge-container">
+      {searchedBadges.map((badge, index) => (
+        <div key={index} className={`badge-card ${mapBadgeToColor(badge.hoursContributed)}`}>
+          <img src="https://via.placeholder.com/100" alt="Badge" />
+          <div className="badge-tooltip">
+            Additional information about the {badge.badgeDescription} badge
+          </div>
+          <div className="badge-name">{badge.badgeDescription}</div>
+          <div className="badge-description">Description: {badge.badgeDescription}</div>
+          <div className="badge-hours">Hours Contributed: {badge.hoursContributed}</div>
         </div>
-      )}
+      ))}
     </div>
+  )}
+</div>
   );
 };
+   
 export default Badges;
+
 
