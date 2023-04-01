271c6813140492e6d392baca2cf0e6e128a361f3
Joseph Del Giorgio <106563909+Joseph-DelGiorgio@users.noreply.github.com>
2023-03-31 17:55:04 -0700

    Update Staking.js

--- my-app/src/components/Staking.js
+++ my-app/src/components/Staking.js
@@ -1,51 +1,81 @@
-import React, { useState } from 'react';
-import './Staking.css';
+import React, { useState } from "react";
 
-const Staking = ({ stakeTokens, unstakeTokens, balance, stakingPosition }) => {
-  const [stakeAmount, setStakeAmount] = useState('');
-  const [unstakeAmount, setUnstakeAmount] = useState('');
+const Staking = ({
+  stakeTokens,
+  unstakeTokens,
+  balance,
+  stakingPosition,
+  createProposal,
+}) => {
+  const [stakeAmount, setStakeAmount] = useState(0);
+  const [unstakeAmount, setUnstakeAmount] = useState(0);
+  const [proposalData, setProposalData] = useState({
+    title: "",
+    description: "",
+    requiredUnicoins: 0,
+  });
 
-  const handleStakeTokens = async () => {
-    await stakeTokens(stakeAmount);
-    setStakeAmount('');
+  const handleStake = () => {
+    stakeTokens(stakeAmount);
   };
 
-  const handleUnstakeTokens = async () => {
-    await unstakeTokens(unstakeAmount);
-    setUnstakeAmount('');
+  const handleUnstake = () => {
+    unstakeTokens(unstakeAmount);
+  };
+
+  const handleProposal = () => {
+    createProposal(proposalData);
   };
 
   return (
-    <div className="staking-container">
+    <div>
       <h2>Staking</h2>
-      <div className="balance">
-        <h3>Your Balance: {balance} UNicoins</h3>
-        <h4>Staked: {stakingPosition} UNicoins</h4>
-      </div>
-      <div className="form-group">
-        <label htmlFor="stakeAmount">Stake Tokens:</label>
+      <div>
         <input
+          className="input-field"
           type="number"
-          id="stakeAmount"
-          className="form-control"
           value={stakeAmount}
           onChange={(e) => setStakeAmount(e.target.value)}
         />
-        <button onClick={handleStakeTokens}>Stake Tokens</button>
+        <button onClick={handleStake}>Stake</button>
       </div>
-      <div className="form-group">
-        <label htmlFor="unstakeAmount">Unstake Tokens:</label>
+      <div>
         <input
+          className="input-field"
           type="number"
-          id="unstakeAmount"
-          className="form-control"
           value={unstakeAmount}
           onChange={(e) => setUnstakeAmount(e.target.value)}
         />
-        <button onClick={handleUnstakeTokens}>Unstake Tokens</button>
+        <button onClick={handleUnstake}>Unstake</button>
+      </div>
+      <p>Your Staking Balance: {balance} Tokens</p>
+      <p>Your Staking Position: {stakingPosition} Tokens</p>
+      <h2>Create Proposal</h2>
+      <div>
+        <input
+          className="input-field"
+          type="text"
+          placeholder="Title"
+          value={proposalData.title}
+          onChange={(e) => setProposalData({ ...proposalData, title: e.target.value })}
+        />
+        <textarea
+          className="input-field description-container"
+          placeholder="Description"
+          value={proposalData.description}
+          onChange={(e) => setProposalData({ ...proposalData, description: e.target.value })}
+        />
+        <input
+          className="input-field"
+          type="number"
+          placeholder="Required UNicoins"
+          value={proposalData.requiredUnicoins}
+          onChange={(e) => setProposalData({ ...proposalData, requiredUnicoins: e.target.value })}
+        />
+        <button onClick={handleProposal}>Create Proposal</button>
       </div>
     </div>
-  );
-};
-
-export default Staking;
+    );
+  };
+  
+  export default Staking;
