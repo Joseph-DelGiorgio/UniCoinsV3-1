f7ae681672fd99ba44b7a5bae7bc8390fa9e5477
Joseph Del Giorgio <106563909+Joseph-DelGiorgio@users.noreply.github.com>
2023-04-01 17:11:07 -0700

    Create FIlterOptions.js

---
+++ my-app/src/components/FIlterOptions.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const FilterOptions = ({ filterAndSortProjects }) => {
+  // You can add more filter and sorting options here
+  return (
+    <div className="filter-options">
+      {/* Add filter and sorting options UI elements here */}
+    </div>
+  );
+};
+
+export default FilterOptions;
