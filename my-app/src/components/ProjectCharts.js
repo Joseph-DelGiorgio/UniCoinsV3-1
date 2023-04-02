aacbffbae29c032fba2e685336a05dc6bfcda65e
Joseph Del Giorgio <106563909+Joseph-DelGiorgio@users.noreply.github.com>
2023-04-01 17:13:22 -0700

    Create ProjectCharts.js

---
+++ my-app/src/components/ProjectCharts.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ProjectCharts = ({ projects }) => {
+  // You can implement chart visualizations using libraries like Chart.js or D3.js here
+  return (
+    <div className="project-charts">
+      {/* Add chart visualizations here */}
+    </div>
+  );
+};
+
+export default ProjectCharts;
