f79d6143f22ecd0996312df69919782fbfa19072
Joseph Del Giorgio <106563909+Joseph-DelGiorgio@users.noreply.github.com>
2023-04-03 07:29:35 -0700

    Update ProjectCharts.js

--- my-app/src/components/ProjectCharts.js
+++ my-app/src/components/ProjectCharts.js
@@ -1,11 +1,44 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Pie } from 'react-chartjs-2';
 
-const ProjectCharts = ({ projects }) => {
-  // You can implement chart visualizations using libraries like Chart.js or D3.js here
+const ProjectCharts = ({ projects = [] }) => {
+  const [chartData, setChartData] = useState(null);
+
+  useEffect(() => {
+    if (
+      !projects.length ||
+      !projects.every(
+        (project) => typeof project === 'object' && project.hasOwnProperty('category')
+      )
+    ) {
+      return;
+    }
+
+    const projectCategories = projects.reduce((acc, project) => {
+      acc[project.category] = (acc[project.category] || 0) + 1;
+      return acc;
+    }, {});
+
+    setChartData({
+      labels: Object.keys(projectCategories),
+      datasets: [
+        {
+          data: Object.values(projectCategories),
+          backgroundColor: [
+            '#FF6384',
+            '#36A2EB',
+            '#FFCE56',
+            '#4BC0C0',
+            '#9966FF',
+            '#FF9F40',
+          ],
+        },
+      ],
+    });
+  }, [projects]);
+
   return (
-    <div className="project-charts">
-      {/* Add chart visualizations here */}
-    </div>
+    <div className="project-charts">{chartData && <Pie data={chartData} />}</div>
   );
 };
 
