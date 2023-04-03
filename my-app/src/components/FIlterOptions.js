6ca29e1577211e5933bc2841251c8d6360a4f938
Joseph Del Giorgio <106563909+Joseph-DelGiorgio@users.noreply.github.com>
2023-04-03 08:43:53 -0700

    Update FIlterOptions.js

--- my-app/src/components/FIlterOptions.js
+++ my-app/src/components/FIlterOptions.js
@@ -1,10 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const FilterOptions = ({ filterAndSortProjects }) => {
-  // You can add more filter and sorting options here
+  const [selectedCategory, setSelectedCategory] = useState('');
+  const [selectedSortOrder, setSelectedSortOrder] = useState('asc');
+
+  // Add more filter and sorting options here if needed
+  const categories = ['', 'Web Development', 'Mobile Development', 'Data Science', 'AI'];
+  const sortOrders = [
+    { value: 'asc', label: 'Ascending' },
+    { value: 'desc', label: 'Descending' },
+  ];
+
+  const handleApplyFilter = () => {
+    filterAndSortProjects(selectedCategory, selectedSortOrder);
+  };
+
   return (
     <div className="filter-options">
-      {/* Add filter and sorting options UI elements here */}
+      <label htmlFor="categoryFilter">Category:</label>
+      <select
+        id="categoryFilter"
+        value={selectedCategory}
+        onChange={(e) => setSelectedCategory(e.target.value)}
+      >
+        {categories.map((category) => (
+          <option key={category} value={category}>
+            {category || 'All Categories'}
+          </option>
+        ))}
+      </select>
+
+      <label htmlFor="sortOrder">Sort Order:</label>
+      <select
+        id="sortOrder"
+        value={selectedSortOrder}
+        onChange={(e) => setSelectedSortOrder(e.target.value)}
+      >
+        {sortOrders.map((order) => (
+          <option key={order.value} value={order.value}>
+            {order.label}
+          </option>
+        ))}
+      </select>
+
+      <button onClick={handleApplyFilter}>Apply Filter</button>
     </div>
   );
 };
